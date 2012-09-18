af51aeba39667dc8155259a95dca0e8a253a4850
Ben Noordhuis <info@bnoordhuis.nl>
2012-09-18 17:13:01 +0200

    reload GDT

--- kern.c
+++ kern.c
@@ -1,3 +1,34 @@
+__attribute__((aligned(8)))
+unsigned char gdt[][8] = {
+  // null descriptor
+  {
+  },
+
+  // 4 GB execute/read code segment, nonconforming
+  {
+    [0] = 255,            // limit
+    [1] = 255,            // limit
+    [2] = 0,              // base
+    [3] = 0,              // base
+    [4] = 0,              // base
+    [5] = 10 | 16 | 128,  // type | non-system | present
+    [6] = 15 | 64 | 128,  // limit | 32 bits | granularity
+    [7] = 0,              // base
+  },
+
+  // 4 GB read/write data segment
+  {
+    [0] = 255,            // limit
+    [1] = 255,            // limit
+    [2] = 0,              // base
+    [3] = 0,              // base
+    [4] = 0,              // base
+    [5] = 2 | 16 | 128,   // type | non-system | present
+    [6] = 15 | 64 | 128,  // limit | 32 bits | granularity
+    [7] = 0,              // base
+  },
+};
+
 __asm__ (
   ".globl mb_init;"
   "mb_init:"
@@ -54,9 +85,31 @@ static void halt(void)
     __asm__ __volatile__ ("cli; hlt");
 }
 
+static void load_gdt(void)
+{
+  const unsigned long base = (unsigned long) &gdt;
+  const unsigned long size = sizeof(gdt) - 1;
+
+  gdt[0][0] = size >> 0;
+  gdt[0][1] = size >> 8;
+  gdt[0][2] = base >> 0;
+  gdt[0][3] = base >> 8;
+  gdt[0][4] = base >> 16;
+  gdt[0][5] = base >> 24;
+  gdt[0][6] = 0;
+  gdt[0][7] = 0;
+
+  __asm__ __volatile__ (
+    "lgdt gdt;"
+    "jmpl $8, $.reload;"
+    ".reload:"
+  );
+}
+
 __attribute__((noreturn))
 void kern_init(void)
 {
-  puts("Hello, world!");
+  load_gdt();
+  puts("Halting.");
   halt();
 }
